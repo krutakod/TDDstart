aadb7916070e0293db6913a0e9fd563942aacf05
Крутаков Евгений <asdurt-soul@yandex.ru>
2022-06-10 19:19:18 +0300

    Init workflows

---
+++ src/test/java/ru/krutakod/tdd/ExTest.java
@@ -0,0 +1,14 @@
+package ru.krutakod.tdd;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class ExTest {
+
+    @Test
+    public void exTestMethod() {
+        assertEquals(1, 1);
+    }
+
+}
